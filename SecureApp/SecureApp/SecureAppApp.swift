b7309f596b972e6e3ef4457627913afe0d274ece
Vijay N <vijn@AIPL6539LT18602.local>
2025-03-22 01:42:30 +0530

    App Switcher Snapshot Controls added

--- SecureApp/SecureApp/SecureAppApp.swift
+++ SecureApp/SecureApp/SecureAppApp.swift
@@ -9,10 +9,28 @@ import SwiftUI
 
 @main
 struct SecureAppApp: App {
+    @State private var isAppInBackground = false
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
-//            DataStorageView(viewModel: DataStorageViewModel())
+                .overlay(
+                    isAppInBackground ? Color.black.ignoresSafeArea() : nil // Cover screen when in background
+                )
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+                    isAppInBackground = true
+                }
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+                    isAppInBackground = false
+                }
         }
     }
 }
+
+
+/*
+ iOS App Security:
+ https://medium.com/adessoturkey/ios-app-security-96c32ba4e036
+ */
+
+
